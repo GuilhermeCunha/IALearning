aa58d3b7b53e25e2c874e5115877af5f902ac496
Vinicius <vinicius.d.paranhos@gmail.com>
2019-09-20 00:05:19 -0300

    perceptron_vinic modificado

--- Projeto/Aulas/Perc_vinic.py
+++ Projeto/Aulas/Perc_vinic.py
@@ -19,7 +19,8 @@ x = np.insert(x, 2, bias, axis=1)
 
 # x_train, x_test, y_train, y_test = np.spl
 
-w = [random.randint(-1, 1), random.randint(-1, 1), random.randint(-1, 1)]
+w = [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
+
 
 taxaApr = 0.05
 
@@ -48,12 +49,10 @@ for epoch in range(0, epochs):
             erros += 1
         if erro:
             for coluna in range(0, len(x[0])):
-                w[coluna] = x[linha][coluna]*(d[linha] - sum)*taxaApr+w[coluna]
-    print("-----------------")
-    print("Epoca ({0}):".format(epoch))
-    print(acertos, erros)
+                w[coluna] = x[linha][coluna]*(d[linha] - sum)*taxaApr + w[coluna]
+    print("--- Epoca ({0}): {1} {2} ---".format(epoch, acertos, erros))
     if(acertos == len(x)):
-        print("Finalizou - epocas: {0}".format(epoch))
+        print("--- Finalizou --- \n--- Epocas: {0} ---".format(epoch))
         break
 
 
@@ -68,17 +67,27 @@ for t in range(len(x)):
 
 xp1 = ((-1) * w[2]) / w[0]
 xp2 = ((-1) * w[2]) / w[1]
-print(str(xp1) + "," + str(xp2))
+# print(str(xp1) + "," + str(xp2))
 
 pontos = [[0, xp1], [xp2, 0]]
 
 z = np.polyfit(pontos[0], pontos[1], 1)
 p = np.poly1d(z)
-print("equação: ", p)
+# print("equação: ", p)
 aux= np.arange(10)
 yaux = p(aux)
-print('Pesos: w1: '+ str(w[0]) + 'w2: '+ str(w[1]) + 'w3: '+ str(w[2]))
+print("--- Pesos ---\n--- w1 = {:.2f} ---".format(w[0]) + "\n--- w2 = {:.2f} ---".format(w[1]) + "\n--- w3 = {:.2f} ---".format(w[2]))
 
 # Generates plot
 plt.plot(aux, yaux, '-', color='black')
 plt.show()
+
+predict = [6.4, 3.2]
+sum = 0
+for coluna in range(0, len(predict)):
+    sum += predict[coluna]*w[coluna]
+print(sum)
+if sum < 0:
+    print("\nSaida = 2")
+else:
+    print("\nSaida = 1")
