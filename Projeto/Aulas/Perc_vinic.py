3092fb6e61083aa439cb4e62c91efdb7efbe7425
Vinicius <vinicius.d.paranhos@gmail.com>
2019-09-27 00:08:17 -0300

    construção do KNN-vinicius - parte de colorar os vizinhos mais próximos em um array

--- Projeto/Aulas/Perc_vinic.py
+++ Projeto/Aulas/Perc_vinic.py
@@ -6,12 +6,15 @@ from csv import reader
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-data = pd.read_csv("/home/viniciusdantz/Documentos/Repositório Github/Python/IALearning/Projeto/Aulas/entrada.txt", sep=" ", names=["comp", "larg", "flor"])
+data = pd.read_csv("/home/viniciusdantz/Documentos/Repositório Github/Python/IALearning/Projeto/Aulas/entrada.txt", sep=" ", names=["comp", "larg", "flor", "dist"])
 
-x = np.array(data.drop(["flor"], 1))
+x = np.array(data.drop(["flor", "dist"], 1))
 
 d = np.array(data["flor"])
+
+data = np.array(data)
 
 bias = 1
 
@@ -74,15 +77,15 @@ pontos = [[0, xp1], [xp2, 0]]
 z = np.polyfit(pontos[0], pontos[1], 1)
 p = np.poly1d(z)
 # print("equação: ", p)
-aux= np.arange(10)
+aux= np.arange(8)
 yaux = p(aux)
 print("--- Pesos ---\n--- w1 = {:.2f} ---".format(w[0]) + "\n--- w2 = {:.2f} ---".format(w[1]) + "\n--- w3 = {:.2f} ---".format(w[2]))
 
 # Generates plot
 plt.plot(aux, yaux, '-', color='black')
-plt.show()
+# plt.show()
 
-predict = [6.4, 3.2]
+""" predict = [6.4, 3.2]
 sum = 0
 for coluna in range(0, len(predict)):
     sum += predict[coluna]*w[coluna]
@@ -90,4 +93,38 @@ print(sum)
 if sum < 0:
     print("\nSaida = 2")
 else:
-    print("\nSaida = 1")
+    print("\nSaida = 1") """
+    
+# print("Larg: \nMax = {0}\nMin = {1}\nComp:\nMax = {2}\nMin = {3}".format(max(data["larg"]), min(data["larg"]), max(data["comp"]), min(data["comp"])))
+'''
+Comp:
+Max = 7.0
+Min = 4.3
+Larg: 
+Max = 4.4
+Min = 2.0
+'''
+
+predict = [random.uniform(0, 10), random.uniform(0, 10)]
+# print(predict)
+
+k = 3 #vizinhos
+
+minors = []
+
+for i in range(len(data)):
+    if(data[i][0] > predict[0]):
+        deltax = data[i][0] - predict[0]
+    else:
+        deltax = predict[0] - data[i][0]
+        
+    if(data[i][1] > predict[1]):
+        deltay = data[i][1] - predict[1]
+    else:
+        deltay = predict[1] - data[i][1]
+    
+    dist = math.sqrt((deltax**2) + (deltay**2))
+    data[i][3] = dist
+    for j in range(k): # parei aqui - parte de colorar os vizinhos mais próximos em um array
+        if(i == 0) minors = [data]
+        if(minors[j] < data):
